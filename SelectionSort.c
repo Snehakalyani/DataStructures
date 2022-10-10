76ff06ce697237efe1c174df29672d244940bff5
SnehaKalyani <snehabaiju2022@gmail.com>
2022-10-10 16:17:46 +0530

    ok

--- SelectionSort.c
+++ SelectionSort.c
@@ -1,41 +1,8 @@
-//bucket numbers (indices): 0, 1, 2, ...99
-n = 100;
-void selectionSort(int *a, int n)
-{
-    int i, imax, t;
-
-for(i =n; i>1; i--)
-{
-    imax = findIndexOfMax(a, i);
-    t = a[imax];
-    a[imax] = a[i-1];
-    a[i-1] = t;
-}
-}
-int findindexOfMax(int i, int *a)
-{
-    int imax;
-    //error case
-    if(n<1)
-    {
-        return -1;
-        }
-imax = 0;
-for(i =1; i <n; i++)
-{
-    if(a[i]> a[imax])
-    {
-        imax = i;
-    }
-}
-return imax;
-}
-
 #include<stdio.h>
 #include<stdlib.h>
 
 void SelectionSort();
-void Findindexofmax();
+int Findindexofmax();
 
 void  main()  
 {  
@@ -49,14 +16,46 @@ void  main()
 	    printf("Enter element %d : ",i + 1);
         scanf("%d", &arr[i]);  
     }  
-  
     printf("\nThe array is: ");  
     for(i=0; i<n; i++)  
     {  
         printf("%d  ", arr[i]);  
     } 
-    	
+    printf("The sorted array is");
+    SelectionSort(arr, n);
+    printf("%d", arr[i]);
 }
+void SelectionSort(int *arr, int n)
+{
+    int i, temp, imaxel;
+   
+    for(i = n; i> 0; i--)
+    {
+        imaxel = Findindexofmax(arr, i);
+        temp = arr[imaxel];
+        arr[imaxel] = arr[i-1];
+        arr[i-1] = temp;
+
+    }
+}
+int Findindexofmax(int *arr, int i)
+{   int n;
+    int imaxel;
+    if(n < 1)
+    {
+        return -1;
+    }
+    imaxel = 0;
+    for(i = 1; i<n; i ++)
+    {
+        if(arr[i]> arr[imaxel])
+        {
+            imaxel = i;
+        }
+    }
+    return imaxel;
+}
+    	
    
 
 
