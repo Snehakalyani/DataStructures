4ea769c1e36fc3d5ff987e55b9502d9ce5723999
SnehaKalyani <snehabaiju2022@gmail.com>
2022-10-17 16:41:27 +0530

    almost correct--just one segmentation fault :(

--- Linkedlist.c
+++ Linkedlist.c
@@ -1,77 +1,46 @@
 #include<stdio.h>
 #include<stdlib.h>
- 
 struct nodeType
     {
     int value; //data
-    
-    struct nodeType *next; //address
-
+    struct nodeType *next; //address(pointer to the next node)
     };
     
 void MakeList(int n);
 void PrintList();
+
+void MakeList(int n)
+{
+int i;
+int value;
+for(i = 1; i < n + 1; i ++)
+{
+    struct nodeType *head = NULL; //head is the first node
+    head = (struct nodeType*)malloc(sizeof(struct nodeType*)); //allocating memory for the first node
+    printf("Enter Data of node %d", i); //user input value of node i
+    scanf("%d", &value);
+    head->value = value ; //putting the value in the value field of node
+    head->next = head; // the next node becomes the head now
+}
+}
+void PrintList()
+{
+    struct nodeType *head;
+    while(head != NULL)
+    {
+    printf("Data= %d", head->value);
+    head->next;
+    }
+}
+
 int main()
 {
     int n;
-    printf("Enter the number of nodes");
-    scanf("%d\n", &n);
+    printf("Enter Size of Linked list");
+    scanf("%d", &n);
     MakeList(n);
     printf("Data in the list");
     PrintList();
     return 0;
     
-}
-
-//Creating a list of n nodes
-
-void MakeList(int n)
-{
-   
-    int i;
-    int value;
-    struct nodeType *head; //defining the head of the first node
-    
-head = (struct nodeType*)malloc(sizeof(struct nodeType*)); //allocating memory for the head of the node
-
-if(head == NULL)
-{
-    printf("No memory allocated");
-    exit(0);
-}
-printf("Enter Data of node 1: "); //user input value of node 1
-scanf("%d\n", &value);
-
-head->value = value ; //putting the value in the value field of node
-head->next = NULL; // address field -->Null
-
-while(n>0)
-{
-    head->next = (struct nodeType*)malloc(sizeof(struct nodeType*));
-    head = head->next;
-    if (head == NULL)
-    {
-        printf("No memory allocated");
-        exit(0);
-    }
-    printf("Enter Data of node %d", i);
-    scanf("%d\n", &value);
-    head-> value = value;
-    head->next = NULL;
-}
-void PrintList();
-{
-    if(head == NULL)
-    {
-        printf("There is no list");
-        exit(0);
-    }
-    while(head != NULL)
-    {
-        printf("Data =%d\n", head->value);
-        head->next;
-    }
-
-}
-
 }
