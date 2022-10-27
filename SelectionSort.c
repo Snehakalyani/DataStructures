159e7d94b6a3ab8a82455d9072f87db0d3e85d0f
SnehaKalyani <snehabaiju2022@gmail.com>
2022-10-28 01:29:48 +0530

    change: variable j included: works now

--- SelectionSort.c
+++ SelectionSort.c
@@ -4,7 +4,39 @@
 void SelectionSort();
 int Findindexofmax();
 
-void  main()  
+
+void SelectionSort(int *arr, int n)
+{
+    int i, temp, imaxel;
+   
+    for(i = n; i> 0; i--)
+    {
+        imaxel = Findindexofmax(arr, i, n);
+        temp = arr[imaxel];
+        arr[imaxel] = arr[i-1];
+        arr[i-1] = temp;
+
+    }
+}
+int Findindexofmax(int *arr, int i, int n)
+{   
+    int imaxel;
+    if(n < 1)
+        {
+            return -1;
+        }
+    imaxel = 0;
+    for(int j = 0; j<i; j ++)
+        {
+            if(arr[j]>arr[imaxel])
+            {
+                imaxel = j;
+            }
+        }
+    return imaxel;
+}
+    	
+ void main()  
 {  
     int arr[20]; 
     int i;
@@ -12,51 +44,23 @@ void  main()
     printf("Enter the Size of array\n");  
     scanf("%d", &n);
     for(i=0; i<n; i++)  
-    {  
-	    printf("Enter element %d : ",i + 1);
-        scanf("%d", &arr[i]);  
-    }  
+        {  
+            printf("Enter element %d : ",i + 1);
+            scanf("%d", &arr[i]);  
+        }  
     printf("\nThe array is: ");  
     for(i=0; i<n; i++)  
-    {  
-        printf("%d  ", arr[i]);  
-    } 
+        {  
+            printf("%d", arr[i]);  
+        } 
     printf("The sorted array is");
     SelectionSort(arr, n);
-    printf("%d", arr[i]);
-}
-void SelectionSort(int *arr, int n)
-{
-    int i, temp, imaxel;
-   
-    for(i = n; i> 0; i--)
-    {
-        imaxel = Findindexofmax(arr, i);
-        temp = arr[imaxel];
-        arr[imaxel] = arr[i-1];
-        arr[i-1] = temp;
-
-    }
-}
-int Findindexofmax(int *arr, int i)
-{   int n;
-    int imaxel;
-    if(n < 1)
-    {
-        return -1;
-    }
-    imaxel = 0;
-    for(i = 1; i<n; i ++)
-    {
-        if(arr[i]> arr[imaxel])
-        {
-            imaxel = i;
-        }
-    }
-    return imaxel;
-}
-    	
-   
+    for(i=0; i<n; i++)  
+        {  
+            printf("%d", arr[i]);  
+        } 
+    
+}  
 
 
     
